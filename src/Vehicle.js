aa1909a669a046b60174b64884d965c7daf7a99a
JeffAllison <itsjallison@gmail.com>
2022-04-27 16:00:12 -0700

    rendered emojis for vehicle types in vehicle.js

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,9 +1,15 @@
-export default function Vehicle() {
+import React from 'react';
+
+export default function Vehicle({ vehicle }) {
   return <div className="vehicle">
     {/* the Vehicle component has a prop called vehicle. Check to see what string is in this vehicle prop */}
     {/* if the vehicle props is a car, render a '🚗' emoji */}
     {/* if the vehicle props is a truck, render a '🚚' emoji */}
     {/* if the vehicle props is a bus, render a '🚌' emoji */}
     {/* if the vehicle props is a motorcycle, render a '🏍️' emoji */}
+    {vehicle.vehicle === 'car' && '🚗'}
+    {vehicle.vehicle === 'truck' && '🚚'}
+    {vehicle.vehicle === 'bus' && '🚌'}
+    {vehicle.vehicle === 'motorcycle' && '🏍️'}
   </div>;
 }
