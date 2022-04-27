36c5e5d6a64c74bcdf0e0225ba0dff4348974b9f
JeffAllison <itsjallison@gmail.com>
2022-04-27 16:19:18 -0700

    added vehiclelist function to .map vehicles

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export default function Vehicle({ vehicle }) {
+export default function Vehicle({ vehicles }) {
   return <div className="vehicle">
     {/* the Vehicle component has a prop called vehicle. Check to see what string is in this vehicle prop */}
     {/* if the vehicle props is a car, render a '🚗' emoji */}
     {/* if the vehicle props is a truck, render a '🚚' emoji */}
     {/* if the vehicle props is a bus, render a '🚌' emoji */}
     {/* if the vehicle props is a motorcycle, render a '🏍️' emoji */}
-    {vehicle.vehicle === 'car' && '🚗'}
-    {vehicle.vehicle === 'truck' && '🚚'}
-    {vehicle.vehicle === 'bus' && '🚌'}
-    {vehicle.vehicle === 'motorcycle' && '🏍️'}
+    {vehicles.vehicle === 'car' && '🚗'}
+    {vehicles.vehicle === 'truck' && '🚚'}
+    {vehicles.vehicle === 'bus' && '🚌'}
+    {vehicles.vehicle === 'motorcycle' && '🏍️'}
   </div>;
 }
